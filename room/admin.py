8206a361ae1d063ead80dd14455973220ea475db
Afjunior18 junior <amauryjuniorvet@yahoo.com.br>
2024-03-04 19:54:01 +0000

    Add price filtering class for room search in admin panel.

--- room/admin.py
+++ room/admin.py
@@ -1,6 +1,32 @@
 from django.contrib import admin
+from django.utils.translation import gettext_lazy as _
 from .models import Room
 from django_summernote.admin import SummernoteModelAdmin
+
+
+
+class PriceRangeFilter(admin.SimpleListFilter):
+    title = _('Price Range')
+    parameter_name = 'price_range'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', _('Less than $500')),
+            ('2', _('$500 - $1000')),
+            ('3', _('$1000 - $1500')),
+            ('4', _('More than $1500')),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '1':
+            return queryset.filter(price__lt=500)
+        elif self.value() == '2':
+            return queryset.filter(price__range=[500, 1000])
+        elif self.value() == '3':
+            return queryset.filter(price__range=[1000, 1500])
+        elif self.value() == '4':
+            return queryset.filter(price__gt=1500)
+
 
 
 
@@ -9,7 +35,7 @@ class RoomAdmin(SummernoteModelAdmin):
 
     list_display = ('room_description', 'room_owner', 'room_availability', 'room_type', 'owner_email', 'owner_phone', 'room_location', 'price', 'created_on', 'status')
     search_fields = ['room_description', 'room_location']
-    list_filter = ('status', 'room_availability', 'room_type')
+    list_filter = ('status', 'room_availability', 'room_type', PriceRangeFilter)
     summernote_fields = ('room_description', 'excerpt')
 
 # Register your models here.
